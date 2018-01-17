5430064a67fe66f14b170f06e8ce802921db299c
root <root@localhost.localdomain>
2018-01-17 16:52:27 +0800

    图片查看移动端适配

--- applications/admin/application/views/housesorders/check_adv_img.php
+++ applications/admin/application/views/housesorders/check_adv_img.php
@@ -1,40 +1,46 @@
 <!-- 加载公用css -->
 <?php $this->load->view('common/header');?>
-
-
-
             <div class="page-content">
 
                 <div class="row">
-                    <form action="" method="post">
                         <div class="col-xs-12">
                             <div class="row-fluid">
-                                <table class="table table-striped table-bordered table-hover">
+                                <table class="table table-striped table-bordered table-hover" >
                                     <thead>
                                         <tr>
-                                        	<th class="center" nowrap>点位编号</th>
-                                        	<th class="center" nowrap>楼盘</th>
-                                            <th class="center" nowrap>组团</th>
-                                            <th class="center" nowrap>楼栋</th>
-                                            <th class="center" nowrap>单元</th>
-                                            <th class="center" nowrap>楼层</th>
-                                            <th class="center" nowrap>点位位置</th>
-                                            <th class="center" nowrap width="175px;">图片</th>
+                                        	<th class="center phone-hide" >点位编号</th>
+                                        	<th class="center phone-hide" >楼盘</th>
+                                            <th class="center phone-hide" >组团</th>
+                                            <th class="center phone-hide" >楼栋</th>
+                                            <th class="center phone-hide" >单元</th>
+                                            <th class="center phone-hide" >楼层</th>
+                                            <th class="center phone-hide" >点位位置</th>
+                                            <th class="center phone-show" >地址</th>
+                                            <th class="center"  >图片</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <?php if(isset($list))foreach ($list as $key => $value) :?>
                                         <tr>
-                                        	<td style="text-align: center;vertical-align: middle;"><?php echo $value['code'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php echo $value['houses_name'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php echo $value['houses_area_name'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php echo $value['ban'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php echo $value['unit'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php echo $value['floor'];?></td>
-                                            <td style="text-align: center;vertical-align: middle;"><?php if(isset($point_addr[$value['addr']])) echo $point_addr[$value['addr']];?></td>
+                                        	<td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['code'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['houses_name'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['houses_area_name'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['ban'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['unit'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php echo $value['floor'];?></td>
+                                            <td class="phone-hide" style="text-align: center;vertical-align: middle;"><?php if(isset($point_addr[$value['addr']])) echo $point_addr[$value['addr']];?></td>
+                                            <td class="phone-show" style="text-align: center;vertical-align: middle;">
+                                            	<?php echo $value['houses_name'];?><br>
+                                            	<?php echo $value['houses_area_name'];?><br>
+                                            	<?php echo $value['ban'];?><br>
+                                            	<?php echo $value['unit'];?><br>
+                                            	<?php echo $value['floor'];?><br>
+                                            	<?php if(isset($point_addr[$value['addr']])) echo $point_addr[$value['addr']];?>
+                                            </td>
                                             <td class="center">
+                                            	<?php if(isset($value['image']) && count($value['image']) > 0): ?>
                                                 <ul class="ace-thumbnails" media-id="<?php echo $value['id'];?>" id="uploader_front_img<?php echo $key;?>">
-                                                    <?php if(isset($value['image']) && count($value['image']) > 0): ?>
+                                                    
                                                         <?php foreach($value['image'] as $val):?>
                                                         <li style="margin:0 auto;">
                                                             <a href="<?php echo $val['front_img'];?>" title="Photo Title" data-rel="colorbox" class="cboxElement">
@@ -43,10 +49,11 @@
                                                             <input type="hidden" name="<?php echo $value['id'];?>[front_img][]" value="<?php echo $val['front_img'];?>"/>
                                                         </li>
                                                         <?php endforeach;?>
-                                                    <?php else:?>
-                                                    	<font style="color:red;">还未上传</font>
-                                                    <?php endif;?>
+                                                    
                                                 </ul>
+                                                <?php else:?>
+                                                    <font style="color:red;">还未上传</font>
+                                                <?php endif;?>
                                             </td>
                                         </tr>
                                         <?php endforeach;?>
@@ -58,7 +65,6 @@
                             </div>
                         </div>
 
-                    </form>
 
                 </div>
             </div>
