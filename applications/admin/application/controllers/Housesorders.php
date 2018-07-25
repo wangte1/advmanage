37e95801a3701026f3004e4720ba1c6760611bde
yonghua <254274509@qq.com>
2018-07-25 11:16:37 +0800

    优化

--- applications/admin/application/controllers/Housesorders.php
+++ applications/admin/application/controllers/Housesorders.php
@@ -1494,23 +1494,24 @@ class Housesorders extends MY_Controller{
                             $point_ids_arr = array_column($canDoList, 'id');
                         }
                     }
-
-                    //释放锁定的客户和占用数
-                    $_result = $this->release($id, $tmp_list['customer_id']);
-                    $where_point['in']['id'] = $point_ids_arr;
-                    $where_point['field']['`ad_num` >'] = '`ad_use_num` + `lock_num`';
-                    $update_data['point_status'] = 1;
-                    $this->Mhouses_points->update_info($update_data, $where_point);
-                    $this->write_log($data['userInfo']['id'], 2, '释放点位：'.$this->db->last_query());
-                    
-                    //移除点位本次的order_id
-                    $res = $this->delOrderIdForPoint($id, $point_ids_arr);
-                    
-                    //更新该订单下所有换画订单的状态为已下画
-                    $order_code = $this->Mhouses_orders->get_one('order_code', array('id' => $id))['order_code'];
-                    $change_count = $this->Mhouses_changepicorders->count(array('order_code' => $order_code));
-                    if ($change_count) {
-                        $this->Mhouses_changepicorders->update_info(array('order_status' => 9), array('order_code' => $order_code));
+                    if(count($point_ids_arr)){
+                        //释放锁定的客户和占用数
+                        $_result = $this->release($id, $tmp_list['customer_id']);
+                        $where_point['in']['id'] = $point_ids_arr;
+                        $where_point['field']['`ad_num` >'] = '`ad_use_num` + `lock_num`';
+                        $update_data['point_status'] = 1;
+                        $this->Mhouses_points->update_info($update_data, $where_point);
+                        $this->write_log($data['userInfo']['id'], 2, '释放点位：'.$this->db->last_query());
+                        
+                        //移除点位本次的order_id
+                        $res = $this->delOrderIdForPoint($id, $point_ids_arr);
+                        
+                        //更新该订单下所有换画订单的状态为已下画
+                        $order_code = $this->Mhouses_orders->get_one('order_code', array('id' => $id))['order_code'];
+                        $change_count = $this->Mhouses_changepicorders->count(array('order_code' => $order_code));
+                        if ($change_count) {
+                            $this->Mhouses_changepicorders->update_info(array('order_status' => 9), array('order_code' => $order_code));
+                        }
                     }
                 }
             }
@@ -1787,8 +1788,8 @@ class Housesorders extends MY_Controller{
     }
     
     /**
-     * @desc 导出Excel
-     */
+      * @desc 导出Excel
+      */
     public function export2() {
         $fields = array('code','houses_name','area_name','ban','unit','floor','addr');
         $res = $this->Mhouses_points->get_lists($fields,array('in' => array('code' => explode(',', $_GET['id']))));
@@ -1803,20 +1804,20 @@ class Housesorders extends MY_Controller{
                 case '3':
                     $res[$k]['addr'] = '地下电梯前室';
                     break;
-            }
+             }
         }
         //加载phpexcel
         $this->load->library("PHPExcel");
         //设置表头
         $table_header =  array(
-            '点位编号' => "code",
-            '所属楼盘' => "houses_name",
-            '所属组团' => 'area_name',
-            '点位楼栋' => 'ban',
-            '点位单元' => 'unit',
-            '点位楼层' => 'floor',
-            '点位位置' => 'addr'
-        );
+                '点位编号' => "code",
+                '所属楼盘' => "houses_name",
+                '所属组团' => 'area_name',
+                '点位楼栋' => 'ban',
+                '点位单元' => 'unit',
+                '点位楼层' => 'floor',
+                '点位位置' => 'addr'
+            );
         $i = 0;
         foreach($table_header as  $k=>$v){
             $cell = PHPExcel_Cell::stringFromColumnIndex($i).'1';
@@ -1833,16 +1834,16 @@ class Housesorders extends MY_Controller{
             }
             $h++;
         }
-        
+                    
         $this->phpexcel->setActiveSheetIndex(0);
         // 输出
         header('Content-Type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename=订单详情换上点位表.xls');
         header('Cache-Control: max-age=0');
-        
+
         $objWriter = PHPExcel_IOFactory::createWriter($this->phpexcel, 'Excel5');
         $objWriter->save('php://output');
-        
+
     }
     
     /*
