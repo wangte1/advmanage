ab6a726606c83c4ed054d180e5e66d33f0c82dba
永华 <254274509@qq.com>
2018-08-01 14:04:28 +0800

    开放花果园的点位选取

--- applications/admin/application/controllers/Housesscheduledorders.php
+++ applications/admin/application/controllers/Housesscheduledorders.php
@@ -812,7 +812,7 @@ class Housesscheduledorders extends MY_Controller{
         if(!empty($this->input->post('houses_id'))) {
             //临时限制不让选花果园和山水黔城
             $houses_id = $this->input->post('houses_id');
-            if(in_array($houses_id, [167, 168])){
+            if(in_array($houses_id, [168])){
                $this->return_json(array('flag' => true, 'points_lists' => [], 'count' => 0, 'area_list' => []));
             }else{
                $where['houses_id'] = $houses_id;
@@ -831,7 +831,7 @@ class Housesscheduledorders extends MY_Controller{
         $where['is_del'] = 0;
         $where['`lock_num` >='] = 0; //防止出现多次选择
         $where['point_status'] = 1;
-        $where['not_in'] = ['houses_id' => [167, 168]];
+        $where['not_in'] = ['houses_id' => [168]];
         
         $fields = 'id,code,houses_id,area_id,ban,unit,floor,addr,type_id,ad_num, ad_use_num, lock_num,point_status';
         $points_lists = $this->Mhouses_points->get_usable_point($fields, $where, $order_id, $type);
