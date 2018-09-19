61862605c98c08d5a711750474104d503873f474
yonghua <254274509@qq.com>
2018-09-19 10:45:59 -0700

    维修列表

--- applications/app/application/controllers/PointReport.php
+++ applications/app/application/controllers/PointReport.php
@@ -9,11 +9,12 @@ class PointReport extends MY_Controller {
     private $token;
     public function __construct() {
         parent::__construct();
-        $this->token = trim($this->input->get_post('token'));
-        $this->doCheckToken($this->token);
+//         $this->token = trim($this->input->get_post('token'));
+//         $this->doCheckToken($this->token);
         $this->load->model([
             'Model_houses_points_report' => 'Mhouses_points_report',
-            'Model_admins' => 'Madmins'
+            'Model_admins' => 'Madmins',
+            'Model_houses_points'=>'Mhouses_points'
         ]);
     }
 
@@ -30,27 +31,52 @@ class PointReport extends MY_Controller {
         $where = ['point_id'=> $point_id];
         $orderBy = ['id' => 'asc'];
         $list = $this->Mhouses_points_report->get_lists("report_img,repair_img,create_id,repair_id,create_time,repair_time,report_msg,remarks",$where, $orderBy, $size, ($page-1)*$size );
-//         echo $this->db->last_query();die;
-        $admin_list = $this->Madmins->get_lists("id,name",['is_del'=>1]);
-        if($list&&$admin_list){
+        $repair_ids = array_unique(array_column($list, 'repair_id')); 
+        $create_ids = array_unique(array_column($list, 'create_id'));
+        $all_admin_ids = array_merge($create_ids, $repair_ids);
+        $adminList = $this->Madmins->get_lists("id,fullname",['in' => ['id' => $all_admin_ids]]);
+        if($list && $adminList){
             foreach ($list as $k=>$v){
                 $list[$k]['create_name'] = '';
                 $list[$k]['report_name'] = ''; 
                 $list[$k]['create_time'] = date('Y-m-d', $v['create_time']);
+                $list[$k]['repair_img'] = get_adv_img($v['repair_img']);
+                $list[$k]['report_img'] = get_adv_img($v['report_img']);
                 if ($list[$k]['repair_time']!=0){
                     $list[$k]['repair_time'] = date('Y-m-d', $v['repair_time']);
                 }
-                foreach ($admin_list as $key => $val){
+                foreach ($adminList as $key => $val){
                     if($val['id'] == $v['create_id']){
-                        $list[$k]['create_name'] = $val['name'];
-                    }
+                        $list[$k]['create_name'] = $val['fullname'];
+                    }  
                     if($val['id'] == $v['repair_id']){
-                        $list[$k]['report_name'] = $val['name'];
-                }        
-            } 
+                        $list[$k]['report_name'] = $val['fullname'];
+                    } 
+            }      
         }  
         $this->return_json(['code' => 1, 'list' =>  $list, 'msg' => "ok"]);    
       }
         $this->return_json(['code' => 0, 'list' => [], 'msg' => "null"]);
+    }
+    
+    /**
+     * 报修列表
+     */
+    public function reportlist(){
+        $pageconfig = C('page.page_lists');
+        $this->load->library('pagination');
+        $page = (int) $this->input->get_post('page') ? : '1';
+        $size = (int) $this->input->get_post('size');
+        if(!$size) $size = $pageconfig['per_page'];
+//         $repair_id = (int) $this->input->get_post('repair_id');
+        $token1 = decrypt($token);
+        $repair_id = $token1['user_id'];
+        $where = ['repair_id'=> $repair_id,"repair_time >"=>0];
+        $orderBy = ['id' => 'asc'];
+        $reportpoint_list = $this->Mhouses_points->get_report_points("A.*,B.repair_id", $where, $orderBy, $size, ($page-1)*$size );
+        if($reportpoint_list){
+            $this->return_json(['code' => 1, 'list' =>  $reportpoint_list, 'msg' => "ok"]);    
+      }
+            $this->return_json(['code' => 0, 'list' => [], 'msg' => "null"]);
     }
 }
--- shared/models/Model_houses_points.php
+++ shared/models/Model_houses_points.php
@@ -896,5 +896,67 @@ class Model_houses_points extends MY_Model {
     	//return (array)$query->result();
     }
     
+    
+    /*
+     * 查询修复列表
+     */
+    
+    public function get_report_points($fields='*', $where = array(), $order_by = array(), $pagesize = 0,$offset = 0,  $group_by = array()){
+        $this->db->select($fields);
+        $this->db->from('t_houses_points A');
+        $this->db->join('t_houses_points_report B','A.id = B.point_id','left');
+        if(isset($where['like'])) {
+            foreach($where['like'] as $k => $v) {
+                $this->db->like($k, $v);
+            }
+            unset($where['like']);
+        }
+        
+        if(isset($where['or_like'])) {
+            foreach($where['or_like'] as $k => $v) {
+                $this->db->or_like($k, $v);
+            }
+            unset($where['or_like']);
+        }
+        
+        if(isset($where['in'])) {
+            foreach($where['in'] as $k => $v) {
+                $this->db->where_in($k, $v);
+            }
+            unset($where['in']);
+        }
+        if(isset($where['not_in'])) {
+            foreach($where['not_in'] as $k => $v) {
+                $this->db->where_not_in($k, $v);
+            }
+            unset($where['not_in']);
+        }
+        
+        if(isset($where['or'])) {
+            $this->db->group_start();
+            foreach($where['or'] as $k => $v) {
+                $this->db->or_where($k, $v);
+            }
+            unset($where['or']);
+            $this->db->group_end();
+        }
+        if($where){
+            $this->db->where($where);
+        }
+        if($order_by) {
+            foreach($order_by as $k => $v) {
+                $this->db->order_by($k, $v);
+            }
+        }
+        if($group_by) {
+            $this->db->group_by($group_by);
+        }
+        if($pagesize > 0) {
+            $this->db->limit($pagesize, $offset);
+        }
+        $result = $this->db->get();
+        return $list = $result->result_array();
+    }  
+    
 
 }
